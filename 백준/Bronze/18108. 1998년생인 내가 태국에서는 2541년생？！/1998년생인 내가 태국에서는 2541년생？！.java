507a71de9d214268f8bc4f7b07a0546592f58e68
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-08-02 16:24:55 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        int input_years = sc.nextInt();
+        int ouput_years = input_years -543;
+
+        System.out.println(ouput_years);
+
+    }
+}
