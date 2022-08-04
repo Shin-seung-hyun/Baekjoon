9e05c7f5b27e815f888af27ae50bda2d233736b3
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-08-04 18:28:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        int money =0;
+
+        if(num1 == num2){
+            if(num2 == num3)  money = 10_000 + num1 * 1_000;
+            else money = 1_000 + num1 * 100;
+        }
+        else if (num1 == num3){
+            money = money = 1_000 + num1 * 100;
+        }
+        else if (num2 == num3){
+            money = money = 1_000 + num2 * 100;
+        }
+        else{
+            if(num1 > num2 && num1 >num3) money = num1 * 100;
+            else if(num2 > num1 && num2 >num3) money = num2 * 100;
+            else money = num3 * 100;
+        }
+
+        System.out.println(money);
+
+    }
+}
