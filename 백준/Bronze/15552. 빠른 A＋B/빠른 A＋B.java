4af9ebbd611a9c3616b837d7935d0a3e3053713e
Seung Hyun Shin <59863297+Shin-seung-hyun@users.noreply.github.com>
2022-08-07 17:53:28 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1136 ms, Memory: 300744 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < cnt; i++){
+            String str = br.readLine();
+            int num1 = Integer.parseInt(str.split(" ")[0]);
+            int num2 = Integer.parseInt(str.split(" ")[1]);
+            bw.write(num1 + num2 + "\n");
+        }
+
+        bw.flush();
+
+    }
+}
